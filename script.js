16b61d2c737774972d64c38617815f4f41dcd2e5
LiviaCCO <livia.ccapistrano@gmail.com>
2023-01-07 23:53:19 -0300

    versão com pedido finalizado - faltando ajustes

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
-let pratoEsc = null;
+let pratoEsc;
 function selecionarPrato(pratoSelecionado){
     const prato = document.querySelector(".prato .selecionado");
     if (prato !== null){
         prato.classList.toggle('selecionado');
     }
-    
     pratoSelecionado.classList.add('selecionado');
 
-    // const check = document.querySelector(".checkIcon .off");
-    // console.log(check);
-    // // if (check !== null){
-    // //     check.classList.add('off');
-    // // }
-    // check.classList.add('off');
+
+    const check = document.querySelector(".prato .escondido");
+    console.log(check);
+    if (check !== null){
+        check.classList.toggle('escondido');
+    }
+    check.classList.remove('escondido');
+
+
     pratoEsc = pratoSelecionado.innerHTML;
     fecharPedido();
 }
 
-let bebidaEsc = null;
+let bebidaEsc;
 function selecionarBebida(bebidaSelecionada){
     const bebida = document.querySelector(".bebida .selecionado");
     if (bebida !== null){
@@ -28,7 +30,7 @@ function selecionarBebida(bebidaSelecionada){
     fecharPedido();
 }
 
-let sobremesaEsc = null;
+let sobremesaEsc;
 function selecionarSobremesa(sobremesaSelecionada){
     const sobremesa = document.querySelector(".sobremesa .selecionado");
     if (sobremesa !== null){
@@ -40,17 +42,70 @@ function selecionarSobremesa(sobremesaSelecionada){
 }
 
 function fecharPedido(){
-    if (pratoEsc !== null && bebidaEsc !== null && sobremesaEsc !== null){
+    if (pratoEsc !== undefined && bebidaEsc !== undefined && sobremesaEsc !== undefined){
         const rodape = document.querySelector(".selecionar");
         rodape.classList.add('escondido');
-        console.log(rodape)
         const rodapeFechar = document.querySelector(".footer .fechar");
-        console.log(rodapeFechar)
         rodapeFechar.classList.remove('escondido');
-        
+        console.log(pratoEsc);
     }
 }
 
-// function msgFinal(){
+let total=0;
+function msgFinal(){
+    const nome = prompt("Qual o seu nome?");
+    const local = prompt("Qual o endereço para entrega?");
+    const dishEsc = document.querySelector(".prato .selecionado .nome");
+    const drinkEsc = document.querySelector(".bebida .selecionado .nome");
+    const dessertEsc = document.querySelector(".sobremesa .selecionado .nome");
+    
+    dish = dishEsc.innerHTML;
+    drink = drinkEsc.innerHTML;
+    dessert = dessertEsc.innerHTML;
 
-// }
+    const priceDishEsc = document.querySelector(".prato .selecionado .preco");
+    const priceDrinkEsc = document.querySelector(".bebida .selecionado .preco");
+    const priceDessertEsc = document.querySelector(".sobremesa .selecionado .preco");
+    
+    // priceDish = Number(priceDishEsc.innerHTML);
+    // priceDrink = Number(priceDrinkEsc.innerHTML);
+    // priceDessert = Number(priceDessertEsc.innerHTML);
+
+    // para trocar , por . 
+    // .replace(",", ".");
+
+    priceDish = Number(priceDishEsc.innerHTML.replace(",", "."));
+    priceDrink = Number(priceDrinkEsc.innerHTML.replace(",", "."));
+    priceDessert = Number(priceDessertEsc.innerHTML.replace(",", "."));
+
+    const total = priceDish + priceDrink + priceDessert;
+    //usando toFixed() para determinar a qtd de casas decimais
+    const price = total.toFixed(2);
+
+    // para transformar número em string
+    // toString()
+    // para trocar , por . : .replace(".", ",")
+    let priceFinal = price.toString().replace(".", ",");
+        
+    let pedido = `Olá, gostaria de fazer o pedido: 
+    
+    - Prato: ${dish} 
+    - Bebida: ${drink} 
+    - Sobremesa: ${dessert} 
+    Total: R$ ${price} 
+       
+    Nome: ${nome} 
+    Endereço: ${local}` ;
+    
+    console.log(pedido);
+    //para abrir página na internet via link
+    function abreLink(){
+        window.open(`https://wa.me/5521970027981?text=${encodeURIComponent(pedido)}`);
+    }
+
+    abreLink();
+}
+    
+
+
+       
