519caa1da13d3abbe10371520483cf707a0c19ae
LiviaCCO <livia.ccapistrano@gmail.com>
2023-01-09 16:46:17 -0300

    versão finalizada - com bonus

--- script.js
+++ script.js
@@ -53,6 +53,8 @@ function fecharPedido(){
         const rodapeFechar = document.querySelector(".footer .fechar");
         rodapeFechar.classList.remove('escondido');
         console.log(pratoEsc);
+
+        // confirmar();    
     }
 }
 
@@ -119,4 +121,126 @@ function msgFinal(){
     
 
 
+
+
+function confirmar(){
+    // para aparecer o resumo final do pedido
+    const resumo = document.querySelector(".paraConfirmar");
+    resumo.classList.remove('escondido');
+
+    //para o nome dos itens:
+    const item1 = document.querySelector(".confirmePrato .item");
+    const item2 = document.querySelector(".confirmeBebida .item");
+    const item3 = document.querySelector(".confirmeSobremesa .item");
+    const dishEsc = document.querySelector(".prato .selecionado .nome");
+    const drinkEsc = document.querySelector(".bebida .selecionado .nome");
+    const dessertEsc = document.querySelector(".sobremesa .selecionado .nome");
+    
+    dish = dishEsc.innerHTML;
+    drink = drinkEsc.innerHTML;
+    dessert = dessertEsc.innerHTML;
+
+    item1.innerHTML = `${dish}`;
+    item2.innerHTML = `${drink}`;
+    item3.innerHTML = `${dessert}`;
+
+
+    //para os valores dos pratos:
+
+    const priceDishEsc = document.querySelector(".prato .selecionado .preco");
+    const priceDrinkEsc = document.querySelector(".bebida .selecionado .preco");
+    const priceDessertEsc = document.querySelector(".sobremesa .selecionado .preco");
+    const valor1 = document.querySelector(".confirmePrato .valor");
+    const valor2 = document.querySelector(".confirmeBebida .valor");
+    const valor3 = document.querySelector(".confirmeSobremesa .valor");
+
+    valorDish = priceDishEsc.innerHTML;
+    valorDrink = priceDrinkEsc.innerHTML;
+    valorDessert = priceDessertEsc.innerHTML;
+    valor1.innerHTML = `${valorDish}`;
+    valor2.innerHTML = `${valorDrink}`;
+    valor3.innerHTML = `${valorDessert}`;
+
+    //para calcular o total:
+    priceDish = Number(priceDishEsc.innerHTML.replace("R$ ", " ").replace(",", "."));
+    priceDrink = Number(priceDrinkEsc.innerHTML.replace("R$ ", " ").replace(",", "."));
+    priceDessert = Number(priceDessertEsc.innerHTML.replace("R$ ", " ").replace(",", "."));
+    
+
+    const total = priceDish + priceDrink + priceDessert;
+    
+    //usando toFixed() para determinar a qtd de casas decimais
+    const valorTotal = total.toFixed(2);
+
+    // para colocar valor final com virgula:
+    const priceFinal = valorTotal.toString().replace(".", ",");
+    const soma = document.querySelector(".soma");
+    soma.innerHTML = `R$ ${priceFinal}`;
+    
+}
+
+function cancelar(){
+    const voltar = document.querySelector(".paraConfirmar");
+    voltar.classList.add('escondido');
+}
+
+
+// function msgFinal(){
+//     const nome = prompt("Qual o seu nome?");
+//     const local = prompt("Qual o endereço para entrega?");
+//     const dishEsc = document.querySelector(".prato .selecionado .nome");
+//     const drinkEsc = document.querySelector(".bebida .selecionado .nome");
+//     const dessertEsc = document.querySelector(".sobremesa .selecionado .nome");
+    
+//     dish = dishEsc.innerHTML;
+//     drink = drinkEsc.innerHTML;
+//     dessert = dessertEsc.innerHTML;
+
+//     const priceDishEsc = document.querySelector(".prato .selecionado .preco");
+//     const priceDrinkEsc = document.querySelector(".bebida .selecionado .preco");
+//     const priceDessertEsc = document.querySelector(".sobremesa .selecionado .preco");
+    
+//     // priceDish = Number(priceDishEsc.innerHTML);
+//     // priceDrink = Number(priceDrinkEsc.innerHTML);
+//     // priceDessert = Number(priceDessertEsc.innerHTML);
+
+//     // para retirar R$ 
+//     // .replace("R$ ", " ")
+//     // para trocar , por . 
+//     // .replace(",", ".");
+ 
+//     priceDish = Number(priceDishEsc.innerHTML.replace("R$ ", " ").replace(",", "."));
+//     priceDrink = Number(priceDrinkEsc.innerHTML.replace("R$ ", " ").replace(",", "."));
+//     priceDessert = Number(priceDessertEsc.innerHTML.replace("R$ ", " ").replace(",", "."));
+    
+
+//     const total = priceDish + priceDrink + priceDessert;
+    
+//     //usando toFixed() para determinar a qtd de casas decimais
+//     const price = total.toFixed(2);
+
+//     // para deixar o número final com vírgula
+//     // // transformar o número em string
+//     // // .toString()
+//     // // para trocar , por . : .replace(".", ",")
+
+//     // let priceFinal = price.toString().replace(".", ",");
+        
+//     let pedido = `Olá, gostaria de fazer o pedido: 
+    
+//     - Prato: ${dish} 
+//     - Bebida: ${drink} 
+//     - Sobremesa: ${dessert} 
+//     Total: R$ ${price} 
        
+//     Nome: ${nome} 
+//     Endereço: ${local}` ;
+    
+//     console.log(pedido);
+//     //para abrir página na internet via link
+//     function abrirLink(){
+//         window.open(`https://wa.me/5521970027981?text=${encodeURIComponent(pedido)}`);
+//     }
+
+//     abrirLink();
+// }
